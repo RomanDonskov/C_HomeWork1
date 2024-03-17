8b52af87bb2a2a0a369de0ff0d4abcc79caaaf4e
RomanDonskov <orix_send@mail.ru>
2024-03-17 15:34:05 +0300

    start commit

---
+++ HomeWork1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello world!\n");
+    printf("Let's\n \t go\n \t\t to walk\n");
+    printf("    *    \n  ***  \n ***** \n*******\n HH HH \n ZZZZZ \n");
+    printf(" __    __ \n/  \\__/  \\\n\\__/  \\__/\n/  \\__/  \\\n\\__/  \\__/\n");
+    return 0;
+}
